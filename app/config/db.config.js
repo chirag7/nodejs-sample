372d693a41a16046ba24a955bdc59dd42888d466
Chirag <chiragudeshi7@gmail.com>
2023-09-26 17:37:19 +0530

    First Commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "sql12.freemysqlhosting.net",
+  USER: "sql12649191",
+  PASSWORD: "eBh1ZQn4Gp",
+  DB: "sql12649191"
+};
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "root",
+//   DB: "node_mysql_example"
+// };
