7b9b1e2e018beffb3be3d1f6c8448dacce9ae3ed
Chirag <chiragudeshi7@gmail.com>
2024-02-15 22:22:51 +0530

    DB config changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  HOST: "sql12.freemysqlhosting.net",
-  USER: "sql12649191",
-  PASSWORD: "eBh1ZQn4Gp",
-  DB: "sql12649191"
+  HOST: "sql6.freemysqlhosting.net",
+  USER: "sql6684315",
+  PASSWORD: "j1LaJ2PJmk",
+  DB: "sql6684315"
 };
+// module.exports = {
+//   HOST: "sql12.freemysqlhosting.net",
+//   USER: "sql12649191",
+//   PASSWORD: "eBh1ZQn4Gp",
+//   DB: "sql12649191"
+// };
 
 // module.exports = {
 //   HOST: "localhost",
